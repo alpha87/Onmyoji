83b88cac04d5a25b42c3ca0ca7c72e9491affaf8
alpha87 <jianxun2004@gmail.com>
2021-03-24 16:14:53 +0800

    秘闻副本不需要等待胜利标志

--- miwen.py
+++ miwen.py
@@ -29,7 +29,7 @@ def run():
 
         # 寻找胜利按钮
         tools.find_img(
-            path=tools.paths["qianshengli"],
+            path=tools.paths["shengli"],
             x=416, y=83, log="点击胜利按钮")
 
         tools.sleep(randint(2, 5))
